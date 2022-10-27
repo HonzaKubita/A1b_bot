b150623cae998227547f81c7512e3eb70376c914
HonzaKubita <honza.kubita@gmail.com>
2022-10-28 01:56:47 +0200

    Removing test and homeworks

---
+++ src/commands/school/odebrattest.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const db = require('../../db');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('odebrattest')
+    .setDescription('odebere test')
+
+    .addIntegerOption(option =>
+      option.setName('id')
+        .setDescription('id testu (př. 5)')
+        .setRequired(true)),
+
+  async execute(interaction, client) {
+
+    const embed = new EmbedBuilder()
+
+    const testId = interaction.options.getInteger('id');
+
+    await db.query("DELETE FROM test WHERE id=$1", [testId], (err, res) => {
+      if (err) {
+        embed
+          .setColor(0xff0000)
+          .setTitle("Nastala chyba při odebírání testu z databáze")
+      } else {
+        embed
+          .setColor(0x00ff00)
+          .setTitle("Test odebrán z databáze")
+      }
+      interaction.reply({
+        embeds:[embed]
+      })
+    });
+
+  }
+}
---
+++ src/commands/school/odebratukol.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+const db = require('../../db');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('odebratukol')
+    .setDescription('odebere úkol')
+
+    .addIntegerOption(option =>
+      option.setName('id')
+        .setDescription('id úkolu (př. 5)')
+        .setRequired(true)),
+
+  async execute(interaction, client) {
+
+    const embed = new EmbedBuilder()
+
+    const homeworkId = interaction.options.getInteger('id');
+
+    await db.query("DELETE FROM homework WHERE id=$1", [homeworkId], (err, res) => {
+      if (err) {
+        embed
+          .setColor(0xff0000)
+          .setTitle("Nastala chyba při odebírání úkolu z databáze")
+      } else {
+        embed
+          .setColor(0x00ff00)
+          .setTitle("Úkol odebrán z databáze")
+      }
+      interaction.reply({
+        embeds:[embed]
+      })
+    });
+
+  }
+}

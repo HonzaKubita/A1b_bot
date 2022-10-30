9304fafed05bc8fba883765aec9d9bf738d7eab9
HonzaKubita <honza.kubita@gmail.com>
2022-10-30 17:06:47 +0100

    sql fixes

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -1,5 +1,21 @@
 -- This file runs everytime you start the database
--- Used for up upgrading the production database
+-- Used for up upgrading the production database that already contains data
+
+CREATE TABLE IF NOT EXISTS "test" (
+	"id" SERIAL PRIMARY KEY,
+	"title" TEXT,
+	"description" TEXT,
+	"subject" TEXT,
+	"date" DATE
+);
+
+CREATE TABLE IF NOT EXISTS "homework" (
+	"id" SERIAL PRIMARY KEY,
+	"title" TEXT,
+	"descriptio" TEXT,
+	"subject" TEXT,
+	"due" DATE
+);
 
 CREATE TABLE IF NOT EXISTS "event" (
 	"id" SERIAL PRIMARY KEY,

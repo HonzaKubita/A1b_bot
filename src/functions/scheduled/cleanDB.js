12bec5c5df400599d4608d853c071e5b85a2d022
HonzaKubita <honza.kubita@gmail.com>
2022-10-30 17:06:02 +0100

    Multiple servers support for cleanDB message

--- src/functions/scheduled/cleanDB.js
+++ src/functions/scheduled/cleanDB.js
@@ -4,7 +4,7 @@ const db = require('../../db');
 module.exports = async (client) => {
   client.cleanDB = async (client) => {
 
-    const channel = await client.channels.cache.get('1035252491241132083');
+    const channels = await client.channels.cache.filter(channel => channel.name === "skola-oznameni");
 
     const embed= new EmbedBuilder()
       .setTitle("[Debug] Databáze vyčištěna od starých úkolů a testů")
@@ -13,8 +13,10 @@ module.exports = async (client) => {
     await db.query("DELETE FROM test WHERE date<current_date");
     await db.query("DELETE FROM homework WHERE due<current_date");
 
-    await channel.send({
-      embeds:[embed]
+    await channels.forEach(channel => {
+      channel.send({
+        embeds:[embed]
+      })
     })
   }
 }
